217a24d02af2096c739ed01ef94c5a0a426872b3
unknown <nsh3vq@virginia.edu>
2014-04-09 12:08:56 -0400

    Moves collection-level logic to cell class

--- cell.py
+++ cell.py
@@ -47,3 +47,24 @@ class Cell:
 		neighbors.append(Cell.getCell(self.x, self.y - 1)) # South
 		neighbors.append(Cell.getCell(self.x - 1, self.y)) # West
 		return neighbors
+
+
+def getCell(x, y):
+	'Return the cell object with coordinates x, y. If none exists, return None'
+	
+	try:
+		if x >= 0 and y >= 0:
+			return cells[x][y]
+		else:
+			return None # prevent wrapping from negative indexing
+	except IndexError:
+		return None # if we're looking for a cell with larger indices than exist, just give None
+
+def makeGraph(w, h):
+	'Instantiate a grid, represented as a 2d list, of new cells of width w and height h'
+
+	cells = [][]
+	for i in range(w):
+		for j in range(h):
+			cells[i][j] = Cell(i, j)
+	return cells
--- maze.py
+++ maze.py
@@ -13,23 +13,3 @@ import pygame
 import cell
 
 WIDTH, HEIGHT, ALGORITHM = sys.argv[1:4] # throw away any extra arguments
-
-def getCell(x, y):
-	'Return the cell object with coordinates x, y. If none exists, return None'
-	
-	try:
-		if x >= 0 and y >= 0:
-			return cells[x][y]
-		else:
-			return None # prevent wrapping from negative indexing
-	except IndexError:
-		return None # if we're looking for a cell with larger indices than exist, just give None
-
-def makeGraph(w, h):
-	'Instantiate a grid, represented as a 2d list, of new cells of width w and height h'
-
-	cells = [][]
-	for i in range(w):
-		for j in range(h):
-			cells[i][j] = Cell(i, j)
-	return cells
